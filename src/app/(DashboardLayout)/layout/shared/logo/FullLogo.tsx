73ca637a336ec6d1a1faf8bad49ebee3d7d20ede
GreenVaccine <blackmask0209@gmail.com>
2025-02-18 02:49:51 -0800

    code formatter

--- src/app/(DashboardLayout)/layout/shared/logo/FullLogo.tsx
+++ src/app/(DashboardLayout)/layout/shared/logo/FullLogo.tsx
@@ -2,23 +2,15 @@
 import React from "react";
 import Image from "next/image";
 import Logo from "/public/favicon.png";
-// import Logowhite from "/public/favicon.png";
 import Link from "next/link";
 const FullLogo = () => {
   return (
     <Link href={"/"}>
-      {/* Dark Logo   */}
       <Image
         src={Logo}
         alt="logo"
         className="block dark:hidden rtl:scale-x-[-1]"
       />
-      {/* Light Logo  */}
-      {/* <Image
-        src={Logowhite}
-        alt="logo"
-        className="hidden dark:block rtl:scale-x-[-1]"
-      /> */}
     </Link>
   );
 };
