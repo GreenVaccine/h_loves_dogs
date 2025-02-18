cdb7dd4296f861612ede994454740fc5b0f43c2e
GreenVaccine <blackmask0209@gmail.com>
2025-02-18 02:48:52 -0800

    code formatter

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,29 +5,27 @@ import { Button } from "flowbite-react";
 import Link from "next/link";
 import type { Metadata } from "next";
 export const metadata: Metadata = {
-  title: "Error-404"
+  title: "Error-404",
 };
 const Error = () => {
   return (
-    <>
-      <div className="h-screen flex items-center justify-center bg-white dark:bg-darkgray">
-        <div className="text-center">
-          <Image src={ErrorImg} alt="error" className="mb-4" />
-          <h1 className="text-ld text-4xl mb-6">Opps!!!</h1>
-          <h6 className="text-xl text-ld">
-            This page you are looking for could not be found.
-          </h6>
-          <Button
-            color={"primary"}
-            as={Link}
-            href="/"
-            className="w-fit mt-6 mx-auto"
-          >
-            Go Back to Home
-          </Button>
-        </div>
+    <div className="h-screen flex items-center justify-center bg-white dark:bg-darkgray">
+      <div className="text-center">
+        <Image src={ErrorImg} alt="error" className="mb-4" />
+        <h1 className="text-ld text-4xl mb-6">Opps!!!</h1>
+        <h6 className="text-xl text-ld">
+          This page you are looking for could not be found.
+        </h6>
+        <Button
+          color={"primary"}
+          as={Link}
+          href="/"
+          className="w-fit mt-6 mx-auto"
+        >
+          Go Back to Home
+        </Button>
       </div>
-    </>
+    </div>
   );
 };
 
